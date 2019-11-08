31557d3d7fc9be07be6906c80e8efa193642e6ca
PC 5 <37745505+vhng@users.noreply.github.com>
2019-11-08 16:24:22 -0300

    V8.1

---
+++ Projeto 15HRS/Assets/Script.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Script : MonoBehaviour
+{
+    void loadScene(string nomeDaCena)
+    {
+         //SceneManagement.loadScene(nomeDaCena);  //Carrega a cena baseada no nome dela
+    }
+}
